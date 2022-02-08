d5be976b90eae25159d335fdfce9d73f311ae9d6
Marcelo Viana <marcelorsviana@gmail.com>
2022-02-07 22:34:53 -0300

    Coding challenge - Android Kotlin Fundamentals 01.2: Anatomy of Basic Android Project - https://developer.android.com/codelabs/kotlin-android-training-app-anatomy#6

--- app/src/main/java/com/mrsv/android/mydiceroller/MainActivity.kt
+++ app/src/main/java/com/mrsv/android/mydiceroller/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.mrsv.android.mydiceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,14 +11,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener{ rollDice() }
+        rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
 //        Toast.makeText(this, "button clicked",
 //        Toast.LENGTH_SHORT).show()
+
         val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
     }
-}
+
+
+    private fun countUp() {
+//        Toast.makeText(this, "button clicked",
+//        Toast.LENGTH_SHORT).show()
+
+        val countUpButton: TextView = findViewById(R.id.result_text)
+
+        if (countUpButton.text == "Hello World!") {
+            countUpButton.text = "1"
+        } else {
+            var countInt: Int = countUpButton.text.toString().toInt()
+
+            if (countInt < 6) {
+                countInt++
+                countUpButton.text = countInt.toString()
+            }
+        }
+    }
+ }
+
+
+
+
+
