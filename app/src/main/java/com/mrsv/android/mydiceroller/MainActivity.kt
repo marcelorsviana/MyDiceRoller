892d3395c9df902933479404635dc6e6a162f9f3
Marcelo Viana <marcelorsviana@gmail.com>
2022-02-09 00:09:21 -0300

    Make vector drawables to be compatible back to API level 7 (app:srcCompat)

--- app/src/main/java/com/mrsv/android/mydiceroller/MainActivity.kt
+++ app/src/main/java/com/mrsv/android/mydiceroller/MainActivity.kt
@@ -4,22 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener{ rollDice() }
+        rollButton.setOnClickListener{ rollDice(diceImage) }
+        diceImage = findViewById(R.id.dice_image)
     }
 
-    private fun rollDice() {
-        val randomInt = (1..6).random()
+    private fun rollDice(diceImage: ImageView) {
+        // Toast.makeText(this, "button clicked",
+        //  Toast.LENGTH_SHORT).show()
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val randomInt = (1..6).random()
 
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
